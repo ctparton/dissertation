76913775cf05629c41b80b46ce5376b56d6d1516
Callum Parton <b7017327@newcastle.ac.uk>
2021-04-27 20:48:08 +0100

    final scripts

--- scripts/face_regression.py
+++ scripts/face_regression.py
@@ -15,98 +15,124 @@ from datetime import date
 from tensorflow.keras.preprocessing import image
 import argparse
 
-
-if __name__ == '__main__':
+def get_param_args():
     parser = argparse.ArgumentParser(description='Pass hyperparameter arguments')
-    parser.add_argument("--b", default=64, help="This is the batch size")
-    parser.add_argument("--layers", default=8, help="This is the trainable layers")
-    parser.add_argument("--lr", default=0.0001, help="This is the learning rate")
+    parser.add_argument("--b", default=10, help="This is the batch size")
+    parser.add_argument("--layers", default=0, help="This is the trainable layers")
+    parser.add_argument("--lr", default=0.001, help="This is the learning rate")
+    parser.add_argument("--type", default='VGG16', help="This is the model architecture (VGG16 or VGGFACE)")
     args = parser.parse_args()
-    print(args)
-    tf.config.list_physical_devices('GPU')
-    BASE_LOG_PATH = './cropped_logs/'
-    BATCH_SIZE = int(args.b)
-    LAST_TRAINABLE_LAYERS = int(args.layers)
-    LR = float(args.lr)
-    MODEL_TYPE = 'regression'
-    TIME = str(date.today()).replace(" ", "-")
-    LOG_DIR = f"{BASE_LOG_PATH}{MODEL_TYPE}/{TIME}_bede_aw_batch_{BATCH_SIZE}_lr_{LR}_2p_layers_{LAST_TRAINABLE_LAYERS}"
-    print(f"Logging to {LOG_DIR}")
-    tensorboard_callback = TensorBoard(log_dir=LOG_DIR)
+    return args
 
+def plotImages(images_arr):
+   fig, axes = plt.subplots(1, 10, figsize=(20, 20))
+   axes = axes.flatten()
+   for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+   plt.tight_layout()
+   plt.show()
+
+def load_data():
     # Preprocess data in csv
     BASE_PATH = Path("../data/regression")
-    CROPPED_BASE_PATH = Path("../data/process/regression")
+    CROPPED_BASE_PATH = Path("../data/process")
     train = pd.read_csv(BASE_PATH / 'train_gt.csv')
     train = train.drop(columns=['stdv'])
-    train["imagepath"] = CROPPED_BASE_PATH / 'train'
-    train["imagepath"] =  train["imagepath"].astype(str) + "/" + train['image']
+    train["imagepath"] = CROPPED_BASE_PATH / 'aligned_train'
+    train["imagepath"] = train["imagepath"].astype(str) + "/" + train['image']
     print(train)
     train = train.astype({"imagepath": 'string'})
     train = train.astype({"image": 'string'})
     train = train.dropna()
     val = pd.read_csv(BASE_PATH / 'valid_gt.csv')
     val = val.drop(columns=["stdv"])
-    val["imagepath"] = CROPPED_BASE_PATH / 'valid'
+    val["imagepath"] = CROPPED_BASE_PATH / 'aligned_valid'
     val["imagepath"] = val["imagepath"].astype(str) + "/" + val['image']
     val = val.astype({"imagepath": 'string'})
     val = val.astype({"image": 'string'})
     val = val.dropna()
+    test = pd.read_csv(CROPPED_BASE_PATH / 'test_gt.csv')
+    test = test.drop(columns=['stdv'])
+    test["imagepath"] = CROPPED_BASE_PATH / 'aligned_test'
+    test["imagepath"] = test["imagepath"].astype(str) + "/" + test['image']
+    test = test.astype({"imagepath": 'string'})
+    test = test.astype({"image": 'string'})
+    test = test.dropna()
+    return (train, val, test)
 
-    train_gen = ImageDataGenerator(preprocessing_function=preprocess_input)
+def construct_model(type):
+    if type == 'VGG16':
+        vgg16_model = VGG16(weights='aug_loose_cropped_model.17-6.47.h5', classes=1)
+        # vgg16_model.summary()
+        model = keras.Sequential()
+        # Take out last classification layer
+        for layer in vgg16_model.layers[:-1]:
+            model.add(layer)
+
+        for layer in model.layers:
+            layer.trainable = False
+
+        # if not LAST_TRAINABLE_LAYERS:
+        #     for layer in model.layers:
+        #         layer.trainable = False
+        # else:
+        #     print(f"Setting trainable layers to {LAST_TRAINABLE_LAYERS}")
+        #     for layer in vgg16_model.layers[:-LAST_TRAINABLE_LAYERS]:
+        #         layer.trainable = False
+        #         print(layer)
+        model.add(Dropout(0.2))
+        model.add(Dense(units=1))
+
+        # Set the last 8 layers to be trainable
+        for layer in model.layers[14:]:
+            layer.trainable = True
+            print(f"{layer}: {layer.trainable}")
+        return model
+    elif type == 'VGGFACE':
+        return None
+    else:
+        return None
+
+if __name__ == '__main__':
+    args = get_param_args()
+    print(args)
+    tf.config.list_physical_devices('GPU')
+    BASE_LOG_PATH = './aligned_logs/'
+    BATCH_SIZE = int(args.b)
+    LAST_TRAINABLE_LAYERS = int(args.layers)
+    LR = float(args.lr)
+    MODEL_TYPE = 'regression'
+    TIME = str(date.today()).replace(" ", "-")
+    LOG_DIR = f"{BASE_LOG_PATH}{MODEL_TYPE}/{TIME}__finalfinal_aligned_loose_aw_batch_{BATCH_SIZE}_lr_{LR}_2p_layers_{LAST_TRAINABLE_LAYERS}"
+    print(f"Logging to {LOG_DIR}")
+    tensorboard_callback = TensorBoard(log_dir=LOG_DIR)
+
+    train, val, test = load_data()
+
+    train_gen = ImageDataGenerator(preprocessing_function=preprocess_input, horizontal_flip=True, width_shift_range=0.1, height_shift_range=0.1, zoom_range=[0.9,1.1], rotation_range=10)
     valid_gen = ImageDataGenerator(preprocessing_function=preprocess_input)
+    test_gen = ImageDataGenerator(preprocessing_function=preprocess_input)
     train_gen = train_gen.flow_from_dataframe(dataframe=train, x_col="imagepath", y_col="mean", class_mode="raw",
                                               target_size=(224, 224), batch_size=BATCH_SIZE)
     valid_gen = valid_gen.flow_from_dataframe(dataframe=val, x_col="imagepath", y_col="mean", class_mode="raw",
                                               target_size=(224, 224), batch_size=BATCH_SIZE)
-
+    test_gen = test_gen.flow_from_dataframe(dataframe=val, x_col="imagepath", y_col="mean", class_mode="raw",
+                                              target_size=(224, 224), batch_size=BATCH_SIZE, shuffle=False)
     imgs, labels = next(train_gen)
 
-    def plotImages(images_arr):
-        fig, axes = plt.subplots(1, 10, figsize=(20, 20))
-        axes = axes.flatten()
-        for img, ax in zip(images_arr, axes):
-            ax.imshow(img)
-            ax.axis('off')
-        plt.tight_layout()
-        plt.show()
-
-    vgg16_model = VGG16(weights='cropped_model.06-6.74.h5', classes=1)
-    # vgg16_model.summary()
-    model = keras.Sequential()
-    # Take out last classification layer
-    for layer in vgg16_model.layers[:-1]:
-        model.add(layer)
-
-    for layer in model.layers:
-        layer.trainable = False
-
-    # if not LAST_TRAINABLE_LAYERS:
-    #     for layer in model.layers:
-    #         layer.trainable = False
-    # else:
-    #     print(f"Setting trainable layers to {LAST_TRAINABLE_LAYERS}")
-    #     for layer in vgg16_model.layers[:-LAST_TRAINABLE_LAYERS]:
-    #         layer.trainable = False
-    #         print(layer)
-    model.add(Dropout(0.4))
-    model.add(Dense(units=1))
-
-    model.compile(optimizer=tf.keras.optimizers.Adam(), loss="mean_absolute_error", metrics=[MeanAbsoluteError()])
-    print(model.summary())
-    model.fit(x=train_gen, steps_per_epoch=len(train_gen), validation_data=valid_gen, validation_steps=len(valid_gen),
-              epochs=50, verbose=2)
-
-    # Set the last 8 layers to be trainable
-    for layer in model.layers[14:]:
-        layer.trainable = True
-        print(f"{layer}: {layer.trainable}")
+    model = construct_model(args.type)
 
     # Recompile model with new learning rate and last 8 layers trainable
     model.compile(optimizer=tf.keras.optimizers.Adam(LR), loss="mean_absolute_error", metrics=[MeanAbsoluteError()])
     print(model.summary())
     model.fit(x=train_gen, steps_per_epoch=len(train_gen), validation_data=valid_gen, validation_steps=len(valid_gen),
-              epochs=50, verbose=2, callbacks=[tensorboard_callback])
+              epochs=50, verbose=2)
+    # callbacks=[tensorboard_callback]
+
+    e = model.evaluate(test_gen, steps=len(test_gen))
+    e = {out: e[i] for i, out in enumerate(model.metrics_names)}
+    print(e)
 
 
     img_array = preprocess_input(keras.preprocessing.image.img_to_array(image.load_img(str(Path('C:\\Uni\\DISS\\project\\data\\test\\000013.jpg')),
--- scripts/visualiser.py
+++ scripts/visualiser.py
@@ -1,90 +1,68 @@
-from io import BytesIO
-
-import requests
-from PIL import Image
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.applications.vgg16 import decode_predictions
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-import numpy as np
-import pandas as pd
-import tensorflow as tf
 from tensorflow import keras
-import matplotlib.pyplot as plt
 from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import Activation, Dense, Dropout
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.losses import MeanAbsoluteError, MeanAbsolutePercentageError
 from pathlib import Path
-from datetime import date
-from tensorflow.keras.preprocessing import image
-import argparse
-from sklearn.model_selection import train_test_split
 from tensorflow.keras import backend as K
 import keract
-import seaborn as sns
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-import copy
-import math
-import pylab
-import seaborn as sns
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras import optimizers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
-from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
-from numpy.random import permutation
-from tensorflow.keras.optimizers import SGD
+from tf_explain.core import OcclusionSensitivity
+
+
+def age_mae(y_true, y_pred):
+    true_age = K.sum(y_true * K.arange(0, 102, dtype="float32"), axis=-1)
+    pred_age = K.sum(y_pred * K.arange(0, 102, dtype="float32"), axis=-1)
+    mae = K.mean(K.abs(true_age - pred_age))
+    return mae
+
+
+def get_saved_model(model_name):
+    model = keras.models.load_model(f'final_models/{model_name}', custom_objects={'age_mae': age_mae})
+    return model
+
+
+def prepare_image(img_path, mode="activation"):
+    pil_image = keras.preprocessing.image.load_img(str(img_path),
+                                                   grayscale=False, target_size=(224, 224))
+    img_array = keras.preprocessing.image.img_to_array(pil_image)
+    if mode == 'activation':
+        img_array = img_array.reshape((1, img_array.shape[0], img_array.shape[1], img_array.shape[2]))
+    img_array = preprocess_input(img_array)
+    img_array /= 255
+    return img_array
+
+
+def predict_on_image(image, model):
+    yhat = model.predict(image)
+    print("Dominant Class: ", np.argmax(yhat))
+    apparent_age = np.round(K.sum(yhat * K.arange(0, 102, dtype="float32"), axis=-1))
+    print("Apparent Age: ", int(apparent_age[0]))
+
+
+def get_layer_outputs(image_path, model):
+    image = prepare_image(image_path)
+    print(predict_on_image(image, model))
+    activations = keract.get_activations(model, image)
+    keract.display_heatmaps(activations, image, directory=f"../visualisations/activation_new/{image_path.name}",
+                            save=True)
+
+
+def visualise_occlusion_sensitivity(image_path, model, target_age):
+    SAVE_PATH = f'../visualisations/occlusion/{image_path.stem}'
+    explainer = OcclusionSensitivity()
+    image = prepare_image(image_path, mode="occlusion")
+    print(image.shape)
+    print(predict_on_image(prepare_image(image_path), model))
+
+    for i in range(3, 30):
+        grid = explainer.explain(([image], None), model, target_age, patch_size=i, colormap=2)
+        explainer.save(grid, SAVE_PATH, f'patch_{i}_age_{target_age}.png')
+
+        grid = explainer.explain(([image], None), model, target_age, patch_size=i)
+        explainer.save(grid, SAVE_PATH, f'default_colour_patch_{i}_age_{target_age}.png')
+
 
 if __name__ == "__main__":
-    # Check for GPUs and set them to dynamically grow memory as needed
-    # Avoids OOM from tensorflow greedily allocating GPU memory
-    vgg16_model = VGG16(weights='final_model.03-3.55.h5', classes=102)
-    vgg16_model.summary()
-    model = keras.Sequential()
-    # Take out last classification layer
-    for layer in vgg16_model.layers[:-1]:
-        model.add(layer)
-
-    # Train only last layer on first pass
-    for layer in model.layers:
-        layer.trainable = False
-    # # if not LAST_TRAINABLE_LAYERS:
-    # #     for layer in model.layers:
-    # #         layer.trainable = False
-    # # else:
-    # #     for layer in vgg16_model.layers[:-LAST_TRAINABLE_LAYERS]:
-    # #         layer.trainable = False
-    #
-    model.add(Dropout(0.4))
-    model.add(Dense(units=102, activation='softmax'))
-    print(model.summary())
-    model.compile(
-        optimizer=keras.optimizers.Adam(1e-3),
-        loss="categorical_crossentropy",
-        metrics=["accuracy"],
-    )
-    image = image.load_img(str(Path('C:\\Uni\\DISS\\project\\data\\test\\000013.jpg')),
-                           grayscale=False, target_size=(224, 224))
-    img_array = keras.preprocessing.image.img_to_array(image)
-    img_array = tf.expand_dims(img_array, 0)
-    yhat = model.predict(img_array)
-    print("most dominant age class (not apparent age): ", np.argmax(yhat))
-    apparent_age = np.round(K.sum(yhat * K.arange(0, 102, dtype="float32"), axis=-1))
-    print("apparent age: ", int(apparent_age[0]))
-
-
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    activations = keract.get_activations(model, image)
-    first = activations.get('block1_conv1')
-    keract.display_heatmaps(activations, image, save=True)
-    keract.display_activations(activations, save=True)
-
+    BASE_PATH = Path("C:\\Uni\\DISS\\project\\data\\process\\aligned\\aligned_test")
+    model = get_saved_model("class_model_nodrop_early_512")
+    visualise_occlusion_sensitivity(Path(BASE_PATH / Path("005651.jpg")), model, 25)
+    visualise_occlusion_sensitivity(Path(BASE_PATH / Path("005913.jpg")), model, 19)
+    visualise_occlusion_sensitivity(Path(BASE_PATH / Path("005681.jpg")), model, 44)
