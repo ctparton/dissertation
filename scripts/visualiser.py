3c54ab0c4cafb5a2400682b3a37f6bf7be69a18d
Callum Parton <b7017327@newcastle.ac.uk>
2021-05-03 20:19:27 +0100

    final

--- scripts/visualiser.py
+++ scripts/visualiser.py
@@ -6,6 +6,88 @@ import keract
 import numpy as np
 from tf_explain.core import OcclusionSensitivity
 from tensorflow.keras.utils import plot_model
+import pylab
+import seaborn as sns
+import math
+import matplotlib.pyplot as plt
+import cv2
+import copy
+
+# Code adapted from https://github.com/oswaldoludwig/Sensitivity-to- occlusion - Keras -
+def visualise_occlusion_sensitivity(image_path, model, occluding_size=70, override_class=None, save_dir='../visualisations/patches/'):
+    '''
+    Produces a heatmap visualisation of sensitivity to occlusion
+
+    :param image_path: file path of the raw image to pass to the model
+    :param model: Keras SavedModel instance
+    :param occluding_size: size of the grey occlusion patch
+    :param override_class: (optional) to produce the heatmap for a particular class
+    :param save_dir: (optional) directory to save result in
+    :return: void
+    '''
+    occluding_stride = 5
+
+    save_path = f"{save_dir}{image_path.stem}"
+    image = cv2.imread(str(image_path))
+    im = prepare_image_open_cv(image)
+    out = model.predict(im)
+    out = out[0]
+
+    if override_class is not None:
+        print("modifying")
+        target_class = override_class
+    else:
+        target_class = np.argmax(out)
+
+    height, width, _ = image.shape
+    output_height = int(math.ceil((height - occluding_size) / occluding_stride + 1))
+    output_width = int(math.ceil((width - occluding_size) / occluding_stride + 1))
+    # init heatmap
+    heatmap = np.zeros((output_height, output_width))
+
+    for h_pixel in range(output_height):
+        for w_pixel in range(output_width):
+            # Size of occlusion patch:
+            h_start = h_pixel * occluding_stride
+            h_end = min(height, h_start + occluding_size)
+
+            w_start = w_pixel * occluding_stride
+            w_end = min(width, w_start + occluding_size)
+
+            # Getting the image copy, applying the occluding window and predicting to obtain prob:
+            input_image = copy.copy(image)
+            input_image[h_start:h_end, w_start:w_end, :] = 128
+            im = prepare_image_open_cv(input_image)
+            out = model.predict(im)
+            # compare to prob without occlusion
+            prob = out[0][target_class]
+            # invert to highlight important areas in warmer tones
+            heatmap[h_pixel, w_pixel] = 1 - prob
+
+    f = pylab.figure()
+    #https://matplotlib.org/stable/tutorials/colors/colormaps.html
+    ax = sns.heatmap(heatmap, cmap='jet', xticklabels=False, yticklabels=False)
+    Path(save_path).mkdir(exist_ok=True, parents=True)
+    if override_class is not None:
+        override = "yes_ov"
+    else:
+        override = "no_ov"
+
+    plt.savefig(f"{save_path}/{occluding_size}_{override}_pos3.png")
+    print('Predicted %s' % target_class)
+
+
+def prepare_image_open_cv(cv_image):
+    '''
+    Alt method to prepare image with opencv
+    :param cv_image: image read using opencv imread
+    :return: processed image
+    '''
+    im = cv2.resize(cv_image, (224, 224)).astype(np.float32)
+    im = np.expand_dims(im, axis=0)
+    im = preprocess_input(im)
+    im /= 255
+    return im
 
 
 def is_valid_model_type(model_type):
@@ -139,8 +221,8 @@ def visualise_layer_outputs(image_path, model, model_type, save_dir='../visualis
         save=True,
     )
 
-
-def visualise_occlusion_sensitivity(image_path, model, target_age, model_type, save_dir='../visualisations/occlusion_test/'):
+# OLD implementation
+def visualise_occlusion_sensitivity_old(image_path, model, target_age, model_type, save_dir='../visualisations/occlusion_test/'):
     """
     Creates an occlusion sensitivity visualisation with varying patch sizes for the given model
 
@@ -176,17 +258,14 @@ if __name__ == "__main__":
     # Visualisations were performed using the classifiction model for compatibility reasons, this model performed
     # similarly to the best performing model
     saved_model = get_saved_model("class_model_nodrop_early_512", MODEL_TYPE)
-
-
-    # visualise_occlusion_sensitivity(Path(BASE_PATH / Path("005651.jpg")), saved_model, 25, MODEL_TYPE)
-    # visualise_layer_outputs(Path(BASE_PATH / Path("005651.jpg")), saved_model, MODEL_TYPE)
-    # visualise_occlusion_sensitivity(Path(BASE_PATH / Path("005913.jpg")), model, 19)
-    # visualise_occlusion_sensitivity(Path(BASE_PATH / Path("005681.jpg")), model, 44)
-    print("005628")
-    prepare_predict(Path(BASE_PATH / Path("005628.jpg")), saved_model, MODEL_TYPE)
-    print("005625")
-    prepare_predict(Path(BASE_PATH / Path("005625.jpg")), saved_model, MODEL_TYPE)
     print("005681")
     prepare_predict(Path(BASE_PATH / Path("005681.jpg")), saved_model, MODEL_TYPE)
-    print("005651")
-    prepare_predict(Path(BASE_PATH / Path("005651.jpg")), saved_model, MODEL_TYPE)
+    visualise_occlusion_sensitivity(Path(BASE_PATH / Path("005681.jpg")), saved_model, occluding_size=30)
+    # print("005628")
+    # prepare_predict(Path(BASE_PATH / Path("005628.jpg")), saved_model, MODEL_TYPE)
+    # print("005625")
+    # prepare_predict(Path(BASE_PATH / Path("005625.jpg")), saved_model, MODEL_TYPE)
+    # print("005681")
+    # prepare_predict(Path(BASE_PATH / Path("005681.jpg")), saved_model, MODEL_TYPE)
+    # print("005651")
+    # prepare_predict(Path(BASE_PATH / Path("005651.jpg")), saved_model, MODEL_TYPE)
