9677bd23e0f3b52e7e3293f151734aeb1b7bc6db
Callum Parton <b7017327@newcastle.ac.uk>
2021-05-05 17:38:30 +0100

    fin

--- util/imdb_wiki_process.py
+++ util/imdb_wiki_process.py
@@ -5,21 +5,36 @@ import csv
 
 
 def remove_poor_samples(no_of_files, dict_from_mat, base_path):
+    """
+    Removes poor samples for IMDB-Wiki
+
+    :param no_of_files: number of samples in the dataset
+    :param dict_from_mat: the dictionary created from the MatLab file
+    :param base_path: where to save the correct samples
+    :return:
+    """
     correct_samples = 0
     for i in range(no_of_files):
-        birth_year = int(dict_from_mat[0][0][i] / 365)  # birth year
-        photo_taken = dict_from_mat[1][0][i]  # photo taken
+        # birth year
+        birth_year = int(dict_from_mat[0][0][i] / 365)
+        # photo taken
+        photo_taken = dict_from_mat[1][0][i]
         path = dict_from_mat[2][0][i][0]
-        face_score = dict_from_mat[6][0][i]  # Face score
-        sec_face_score = dict_from_mat[7][0][i]  # Sec face score
+        # Face score
+        face_score = dict_from_mat[6][0][i]
+        # Sec face score
+        sec_face_score = dict_from_mat[7][0][i]
 
         age = photo_taken - birth_year
 
         face_score = str(face_score)
         sec_face_score = str(sec_face_score)
 
-        if 'n' not in face_score:  # n as in Inf; if true, implies that there isn't a face in the image
-            if 'a' in sec_face_score:  # a as in NaN; implies that no second face was found
+        # Check for Inf; if true, implies that there isn't a face in the image
+        if 'n' not in face_score:
+            # Check for NaN; if True, no second face in image
+            if 'a' in sec_face_score:
+                # Check age
                 if 0 <= age <= 101:
                     correct_samples += 1
 
@@ -40,6 +55,11 @@ def remove_poor_samples(no_of_files, dict_from_mat, base_path):
 
 
 def count_total_files(dir_path):
+    """
+    Counts the total files in a directory
+    :param dir_path: directory to the images
+    :return: number of files
+    """
     total_files = 0
     has_subdir = not any([x.is_file() for x in dir_path.iterdir() if x.suffix != '.mat'])
     print(f"Subdirectories in {dir_path} = {has_subdir}")
@@ -55,6 +75,13 @@ def count_total_files(dir_path):
 
 
 def build_csv(dir_path, train_size):
+    """
+    Creates a CSV, which can be read in during training
+
+    :param dir_path: path to save the csv
+    :param train_size: portion of samples to label as training data
+    :return: void
+    """
     csv_label_file = Path(dir_path / 'imdb_wiki_labels_t60.csv')
     if csv_label_file.is_file() and csv_label_file.exists():
         print(f"{csv_label_file} already exists")
@@ -79,23 +106,28 @@ def build_csv(dir_path, train_size):
         row_count = sum(1 for row in output_file)
         print(f"Total Rows {row_count}")
 
+def load_label_data():
+    """
+    Loads matlab files for the IMDB and wiki datasets into a dict
+
+    :return: Tuple of dictionaries
+    """
+   return (loadmat(BASE_PATH_IMDB / 'imdb.mat')['imdb'][0][0], loadmat(BASE_PATH_WIKI / 'wiki.mat')['wiki'][0][0])
 
 if __name__ == '__main__':
     BASE_PATH_IMDB = Path('C:\\Users\\CallumDesk\\Downloads\\imdb_crop')
     BASE_PATH_WIKI = Path('C:\\Users\\CallumDesk\\Downloads\\wiki_crop')
     PROCESSED_FILE_PATH = (Path('C:\\Users\\CallumDesk\\Downloads\\imdb_wiki_processed'))
-    train_split = 418440  # 80% training split
     train_dest = Path(BASE_PATH_IMDB / 'train')
     val_dest = Path(BASE_PATH_IMDB / 'validation')
 
     imdb_total_files = count_total_files(BASE_PATH_IMDB)
     wiki_total_files = count_total_files(BASE_PATH_WIKI)
     print(f"IMDB Total: {imdb_total_files} WIki Total: {wiki_total_files}")
-    imdb_metadata = loadmat(BASE_PATH_IMDB / 'imdb.mat')['imdb'][0][0]
-    wiki_metadata = loadmat(BASE_PATH_WIKI / 'wiki.mat')['wiki'][0][0]
+    imdb_metadata, wiki_metadata = load_label_data()
 
-    # print(remove_poor_samples(imdb_total_files, imdb_metadata, BASE_PATH_IMDB))
-    # print(remove_poor_samples(wiki_total_files, wiki_metadata, BASE_PATH_WIKI))
+    print(remove_poor_samples(imdb_total_files, imdb_metadata, BASE_PATH_IMDB))
+    print(remove_poor_samples(wiki_total_files, wiki_metadata, BASE_PATH_WIKI))
     processed_total_files = count_total_files(PROCESSED_FILE_PATH)
     print(f"Total processed files: {processed_total_files}")
     print(f" 70% train split: {int(processed_total_files * 0.60)}")
